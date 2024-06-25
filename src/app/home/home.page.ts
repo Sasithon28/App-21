be4517d957efd76618bc6552c92c060828bf1d76
Sasithon28 <suchanatch2288@gmail.com>
2024-06-25 14:25:59 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  //ตัวแปรรับค่าจาก input
+  weight ? : number;
+  hight ? : number;
 
+  //ตัวแปรที่ใช้ในการแสดงผลลัพธ์
+  result ? : number;
+  result_text ? : string ="";
   constructor() {}
+
+  oncal(){
+    console.log("w :" + this.weight)
+    console.log("h :" + this.hight)
+    if (this.weight == undefined || this.hight == undefined) {
+    alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+    return;
+  }
+    var hi =this.hight /100;
+    this.result = this.weight / (hi*hi);
+    console.log("ผลลัพธ์" + this.result)
+    
+
+    if(this.result <18.5) {
+      //console.log("ผอมเกินไป");
+      this.result_text = "ผอมเกินไป";
+    }else if(this.result >=18.5 && this.result <=24){
+      //console.log("ปกติ เหมาะสม");
+      this.result_text = "ปกติ เหมาะสม";
+    }else if(this.result >=25 && this.result <=29.9){
+      //console.log("อ้วน");
+      this.result_text = "อ้วน";
+    }else{
+      //console.log("อ้วนมาก");
+      this.result_text = "อ้วนมาก";
+    }
+  }
+
+  
 
 }
